e202558cddc185f67c1a8cb1a448b7bff1a66474
Donovan <donovanle18@gmail.com>
2020-10-20 12:37:46 -0500

    initial commit

---
+++ src/bookmark/bookmarks-router.js
@@ -0,0 +1,80 @@
+const express = require("express");
+
+const bodyParser = express.json();
+const bookmarkRouter = express.Router();
+const { v4: uuid } = require("uuid");
+const logger = require("../logger");
+const { bookmarks } = require("../store");
+
+bookmarkRouter.get("/", (req, res) => {
+    res.json(bookmarks);
+});
+
+bookmarkRouter.post("/", bodyParser, (req, res) => {
+    const { title, rating, desc, url } = req.body;
+
+    if (!title) {
+        logger.error("TITLE IS pho-king REQUIRED");
+        return res.status(400).send("TITLE IS REQUIRED");
+    }
+    if (!rating) {
+        logger.error("RATING IS pho-king REQUIRED");
+        return res.status(400).send("RATING IS REQUIRED");
+    }
+    if (!desc) {
+        logger.error("Description IS pho-king REQUIRED");
+        return res.status(400).send("Description IS REQUIRED");
+    }
+    if (!url) {
+        logger.error("URL IS pho-king REQUIRED");
+        return res.status(400).send("URL IS REQUIRED");
+    }
+
+    const id = uuid();
+
+    const bookmark = {
+        id,
+        title,
+        rating,
+        desc,
+        url,
+    };
+
+    bookmarks.push(bookmark);
+    logger.info(`Card with ${id} id has been made`);
+    res.status(201)
+        .location(`http://localhost:8000/bookmarks/${id}`)
+        .json(bookmarks);
+});
+
+bookmarkRouter.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const bookmark = bookmarks.find((x) => x.id === id);
+
+    if (!bookmark) {
+        logger.error(`Bookmark with ${id} id was not found`);
+        return res.status(400).send("BOOKMARK NOT FOUND");
+    }
+
+    res.status(200).send(bookmark);
+});
+
+bookmarkRouter.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const bookmarkIndex = bookmarks.findIndex((x) => x.id == id);
+    console.log(bookmarkIndex);
+    if (bookmarkIndex === -1) {
+        logger.error(`Bookmark ${id} not found`);
+        return res.status(400).send("BOOKMARK NOT FOUND");
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with ${id} id has been deleted`);
+
+    res.status(201).json(bookmarks);
+});
+
+module.exports = bookmarkRouter;
---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+    {
+        id: "1",
+        title: "Amazon",
+        rating: 5,
+        desc: "Online Retailer",
+        url: "https://www.amazon.com/",
+    },
+];
+
+module.exports = { bookmarks };
